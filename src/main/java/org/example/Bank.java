b64ecd637cdbb9853046346bf6806463958540db
plominskai <plominska.izabela@gmail.com>
2023-06-05 21:05:06 +0200

    created payoff and paycheck for bank_app

--- src/main/java/org/example/Bank.java
+++ src/main/java/org/example/Bank.java
@@ -2,8 +2,11 @@ package org.example;
 
 public class Bank {
 
-    double balance = 0;
+    public double balance = 0;
     public double deposit(double payment) {
         return balance = balance + payment;
+    }
+    public double payoff(double paycheck){
+        return balance = balance - paycheck;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Bank bank = new Bank();
-        bank.deposit(2000);
-        System.out.println(bank.deposit(1000));
+        Scanner scanner = new Scanner(System.in);
+        double money = scanner.nextDouble();
+        bank.deposit(money);
+        System.out.println("After payment: " + bank.deposit(1000));
+        bank.payoff(500);
+        System.out.println("After paycheck: " + bank.payoff(100));
     }
 }
