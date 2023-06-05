e1b8a1b8f93f96f263118ac67c2975d4566877f8
plominskai <plominska.izabela@gmail.com>
2023-06-05 20:38:15 +0200

    added bank_app, created deposit, payment and balance

---
+++ src/main/java/org/example/Bank.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Bank {
+
+    double balance = 0;
+    public double deposit(double payment) {
+        return balance = balance + payment;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.add(4, 5));
-        System.out.println(calculator.sub(0.5, 5));
-
+        Bank bank = new Bank();
+        bank.deposit(2000);
+        System.out.println(bank.deposit(1000));
     }
 }
